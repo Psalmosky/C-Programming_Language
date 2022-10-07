221f7fad50f2282543fd1265407d96c8fa7e4a67
Psalmosky <oluwaseyiomotayo1@gmail.com>
2022-10-07 02:25:33 -0700

    Print the max and min between three numbers

---
+++ Project_11/3-max_num.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Find the max and min number between 3 integers
+ */
+
+int main()
+{
+	int a, b, c;
+	int max, min;
+
+	printf("Enter the first number: ");
+	scanf("%d", &a);
+
+	printf("Enter the second number: ");
+	scanf("%d", &b);
+
+	printf("Enter the third number: ");
+	scanf("%d", &c);
+
+	max = a;
+	min = b;
+
+	if (a < b)
+	{
+		max = b;
+		min = a;
+	}
+	
+	if (b < c)
+	{
+		max = c;
+	}
+
+	if (c < b)
+		min = c;
+
+	printf("The Maximum number between the 3 numbers = %d \n", max);
+	printf("The Minimum number between the 3 numbers = %d \n", min);
+
+	return 0;
+}
